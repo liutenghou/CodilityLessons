d811d44de871c747e0d127a56244a993484a4414
Leo Liu <leo.liu@aequilibrium.ca>
2018-12-05 20:38:10 -0800

    lesson 3: perm missing element

--- src/CodilityPractice.java
+++ src/CodilityPractice.java
@@ -1,13 +1,44 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-public class BinaryGap {
+public class CodilityPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing");
 //		System.out.println(findBinaryGap(133342));
 		int[] input = {1,2,3,4};
-		System.out.println(rotateRight(input, 3));
+//		System.out.println(rotateRight(input, 3));
+		int[] input2 = {2};
+		System.out.println(permMissingElem(input2));
+	}
+	
+	static int permMissingElem(int[] a) {
+		int missingNo = 0;
+		int aSize = a.length;
+		//edge cases
+		if(aSize < 1 || a == null) {
+			return 1;
+		}
+		int[] allA = new int[aSize + 2];
+		for(int i=0; i<aSize; i++) {
+			int val = a[i];
+			allA[val] = val;
+		}
+		System.out.println("new array: " + java.util.Arrays.toString(allA));
+		
+		missingNo = 1;
+		for(int i=1; i<allA.length; i++) {
+			if(allA[i] == 0) {
+				break;
+			}else {
+				missingNo++;
+			}
+		}
+		return missingNo;
 	}
 	
 	static int[] rotateRight(int[] preRotateArray, int timesToRotate) {
